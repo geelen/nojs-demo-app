0489920e730b6fcf68fdf6058a68900530b83c9e
Glen Maddern <glenmaddern@gmail.com>
2013-09-12 16:06:59 +1000

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('nojsDemoAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
